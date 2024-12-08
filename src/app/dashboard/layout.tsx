58e22752eaa213560893abefa2608ec5463b27fb
Darwin Ruiz <darwinorlandoruiz15@gmail.com>
2024-12-07 22:45:34 -0600

    Initial commit: Sending source code to remote repository

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+
+export default function DashboardLayout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+            <div className="flex ">
+
+
+
+                <div className="p-2 h-full text-slate-900">
+
+                    {children}
+                </div>
+
+            </div>
+        </div>
+    );
+}
